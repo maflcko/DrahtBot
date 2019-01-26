8713dace66d7689b50992c1f106985292fc67974
MarcoFalke <falke.marco@gmail.com>
2019-01-25 20:03:45 -0500

    coverage: Run on 9 most recent pulls

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -279,7 +279,7 @@ def main():
     print('Fetching open pulls ...')
     github_api = Github(args.github_access_token)
     repo_code = github_api.get_repo(args.repo_code)
-    pulls = return_with_pull_metadata(lambda: [p for p in repo_code.get_pulls(state='open')][:3])
+    pulls = return_with_pull_metadata(lambda: [p for p in repo_code.get_pulls(state='open')][:9])
     call_git(['fetch', '--quiet', '--all'])  # Do it again just to be safe
     call_git(['fetch', 'origin', '{}'.format(args.base_name), '--quiet'])
     pulls = [p for p in pulls if p.base.ref == args.base_name]
