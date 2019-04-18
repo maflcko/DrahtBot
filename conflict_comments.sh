d889b7a282d07e2d017d6fae10a4ef5bb8e2b787
MarcoFalke <falke.marco@gmail.com>
2019-04-18 11:57:49 -0400

    conflicts: Merge sh scripts

--- conflict_comments.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-source ./env_3/bin/activate && python3 ./scripts/conflicts.py --update_comments --github_access_token  b6005448c263761141325e7bf756d14ce06ce34d "$@"
--- conflicts.sh
+++ conflicts.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-source ./env_3/bin/activate && python3 ./scripts/conflicts.py --github_access_token b6005448c263761141325e7bf756d14ce06ce34d --pull_id "${@}"
+source ./env_3/bin/activate && python3 ./scripts/conflicts.py --github_access_token b6005448c263761141325e7bf756d14ce06ce34d "${@}"
