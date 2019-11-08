52146e80e86aee83312677de7e9180c895ab60c9
MarcoFalke <falke.marco@gmail.com>
2019-11-08 14:13:14 -0500

    gitian: Check out fafa06d9b5df590123922456aad2e72641214e4a (linux64 arch fix)

--- scripts/gitian-build.py
+++ scripts/gitian-build.py
@@ -23,7 +23,7 @@ def setup():
     if not os.path.isdir('bitcoin'):
         subprocess.check_call(['git', 'clone', 'https://github.com/bitcoin/bitcoin.git'])
     os.chdir('gitian-builder')
-    subprocess.check_call(['git', 'checkout', '4f269d4afecbf517c0d16927ba99e0f136614743~1'])
+    subprocess.check_call(['git', 'checkout', 'fafa06d9b5df590123922456aad2e72641214e4a'])
     make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64']
     if args.docker:
         make_image_prog += ['--docker']
