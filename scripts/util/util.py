6ab7e3316eb9d7d3fd7a68e59269e4493fb6aa7f
MarcoFalke <falke.marco@gmail.com>
2020-04-28 22:03:48 -0400

    gitian table: More agressive regex to also cover targz

--- scripts/util/util.py
+++ scripts/util/util.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from enum import Enum, unique
 import re
+import os
 import subprocess
 
 UPSTREAM_PULL = 'upstream-pull'
@@ -80,14 +81,14 @@ def return_with_pull_metadata(get_pulls):
 def calculate_table(base_folder, commit_folder, external_url, base_commit, commit):
     rows = defaultdict(lambda: ['', ''])  # map from abbrev file name to list of links
     for f in sorted(os.listdir(base_folder)):
-        short_file_name = re.sub(r'-[a-f0-9]{10}-', '-*-', f)
+        short_file_name = re.sub(r'[a-f0-9]{10}', '*', f)
         os.chdir(base_folder)
         left = rows[short_file_name]
         left[0] = '[`{}...`]({}{}/{})'.format(subprocess.check_output(['sha256sum', f], universal_newlines=True)[:16], external_url, base_commit, f)
         rows[short_file_name] = left
 
     for f in sorted(os.listdir(commit_folder)):
-        short_file_name = re.sub(r'-[a-f0-9]{10}-', '-*-', f)
+        short_file_name = re.sub(r'[a-f0-9]{10}', '*', f)
         os.chdir(commit_folder)
         right = rows[short_file_name]
         right[1] = '[`{}...`]({}{}/{})'.format(subprocess.check_output(['sha256sum', f], universal_newlines=True)[:16], external_url, commit, f)
