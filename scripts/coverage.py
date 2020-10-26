13b1147fe53905125dc2746f1c84a55bfb63b5f9
MarcoFalke <falke.marco@gmail.com>
2020-10-26 10:10:54 +0100

    coverage: Remove confusing copyright header

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-# Copyright (c) 2014-2018 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 from github import Github, GithubException
 import time
 import datetime
