fe4104c266136415d979282d22f79f82d7f88bbd
MarcoFalke <falke.marco@gmail.com>
2020-11-17 12:48:14 +0100

    fuzz_gen: Require loop outside of script

--- scripts/fuzz_gen.py
+++ scripts/fuzz_gen.py
@@ -33,25 +33,24 @@ def main():
         print('Clone {} repo to {}'.format(url_seed, dir_assets))
         call_git(['clone', '--quiet', url_seed, dir_assets])
 
-    while True:
-        print('Fetch upsteam, checkout latest branch')
-        os.chdir(dir_code)
-        call_git(['fetch', '--quiet', '--all'])
-        call_git(['checkout', 'origin/master'])
-        call_git(['reset', '--hard', 'HEAD'])
-        call_git(['clean', '-dfx'])
-        subprocess.check_call(['sed', '-i', 's/runs=100000/use_value_profile=1","-max_total_time=6000/g', 'test/fuzz/test_runner.py'])
+    print('Fetch upsteam, checkout latest branch')
+    os.chdir(dir_code)
+    call_git(['fetch', '--quiet', '--all'])
+    call_git(['checkout', 'origin/master'])
+    call_git(['reset', '--hard', 'HEAD'])
+    call_git(['clean', '-dfx'])
+    subprocess.check_call(['sed', '-i', 's/runs=100000/use_value_profile=1","-max_total_time=6000/g', 'test/fuzz/test_runner.py'])
 
-        os.chdir(dir_assets)
-        call_git(['fetch', '--quiet', '--all'])
-        call_git(['checkout', 'origin/master'])
+    os.chdir(dir_assets)
+    call_git(['fetch', '--quiet', '--all'])
+    call_git(['checkout', 'origin/master'])
 
-        os.chdir(dir_code)
-        subprocess.check_call(f'./autogen.sh && CC=clang CXX=clang++ ./configure --enable-fuzz --with-sanitizers=address,fuzzer,undefined && make clean && make -j {args.jobs}', shell=True)
-        shutil.rmtree(dir_generate_seeds, ignore_errors=True)
-        subprocess.check_call([sys.executable, 'test/fuzz/test_runner.py', '-l=DEBUG', f'--par={args.jobs}', f'{dir_generate_seeds}', f'--m_dir={dir_assets}/fuzz_seed_corpus'])
-        subprocess.check_call([sys.executable, 'test/fuzz/test_runner.py', '-l=DEBUG', f'--par={args.jobs}', f'{dir_generate_seeds}', '--generate'])
-        subprocess.check_call([sys.executable, 'test/fuzz/test_runner.py', '-l=DEBUG', f'--par={args.jobs}', f'{dir_assets}/fuzz_seed_corpus', f'--m_dir={dir_generate_seeds}'])
+    os.chdir(dir_code)
+    subprocess.check_call(f'./autogen.sh && CC=clang CXX=clang++ ./configure --enable-fuzz --with-sanitizers=address,fuzzer,undefined && make clean && make -j {args.jobs}', shell=True)
+    shutil.rmtree(dir_generate_seeds, ignore_errors=True)
+    subprocess.check_call([sys.executable, 'test/fuzz/test_runner.py', '-l=DEBUG', f'--par={args.jobs}', f'{dir_generate_seeds}', f'--m_dir={dir_assets}/fuzz_seed_corpus'])
+    subprocess.check_call([sys.executable, 'test/fuzz/test_runner.py', '-l=DEBUG', f'--par={args.jobs}', f'{dir_generate_seeds}', '--generate'])
+    subprocess.check_call([sys.executable, 'test/fuzz/test_runner.py', '-l=DEBUG', f'--par={args.jobs}', f'{dir_assets}/fuzz_seed_corpus', f'--m_dir={dir_generate_seeds}'])
 
 
 if __name__ == '__main__':
