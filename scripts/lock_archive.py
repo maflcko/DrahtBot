bb512d160d41ea6f2cef8716dfe4b4b5ae7ccab1
MarcoFalke <falke.marco@gmail.com>
2021-09-08 14:15:52 +0200

    lock_archive: Check issues as well

--- scripts/lock_archive.py
+++ scripts/lock_archive.py
@@ -1,4 +1,4 @@
-from github import Github, GithubException
+from github import Github, GithubException, PullRequest
 import argparse
 
 from util.util import return_with_pull_metadata
@@ -16,21 +16,22 @@ def main():
 
     github_api = Github(args.github_access_token)
     for github_repo in [github_api.get_repo(r) for r in args.github_repo.split(',')]:
+        for getter in (github_repo.get_pulls, github_repo.get_issues):
 
-        print(f'Get closed pulls for repo {github_repo.owner.login}/{github_repo.name} ...')
+            print(f'{getter.__name__} (closed) for repo {github_repo.owner.login}/{github_repo.name} ...')
 
-        for i, p in enumerate(github_repo.get_pulls(state='closed', direction='asc', sort='updated')):
-            print(f'Checking pull number #{p.number} from year {p.updated_at.year} against {args.year}')
-            if p.updated_at.year > args.year:
-                print(f'All done up to year {args.year}')
-                break
-            issue = p.as_issue()
-            if issue.locked:
-                print('Already locked')
-                continue
-            print(f'{p}\n    .lock({LOCK_REASON})')
-            if not args.dry_run:
-                issue.lock(LOCK_REASON)
+            for el in getter(state='closed', direction='asc', sort='updated'):
+                print(f'Checking number #{el.number} from year {el.updated_at.year} against {args.year}')
+                if el.updated_at.year > args.year:
+                    print(f'All done up to year {args.year}')
+                    break
+                issue = el.as_issue() if type(el) is PullRequest.PullRequest else el
+                if issue.locked:
+                    print('Already locked')
+                    continue
+                print(f'{el}\n    .lock({LOCK_REASON})')
+                if not args.dry_run:
+                    issue.lock(LOCK_REASON)
 
 
 if __name__ == '__main__':
