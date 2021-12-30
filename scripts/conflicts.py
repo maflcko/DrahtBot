5363d562193de039edff6374f5a69174bdc42792
MarcoFalke <falke.marco@gmail.com>
2021-12-30 12:03:20 +0100

    conflicts: slug list + base_name

--- scripts/conflicts.py
+++ scripts/conflicts.py
@@ -65,77 +65,82 @@ def main():
     parser.add_argument('--update_comments', action='store_true', help='Update all conflicts comments and labels.', default=False)
     parser.add_argument('--scratch_dir', help='The local dir used for scratching', default=os.path.join(THIS_FILE_PATH, '..', 'scratch', 'conflicts'))
     parser.add_argument('--github_access_token', help='The access token for GitHub.', default='')
-    parser.add_argument('--github_repo', help='The repo slug of the remote on GitHub.', default='bitcoin/bitcoin')
-    parser.add_argument('--base_name', help='The name of the base branch.', default='master')
+    parser.add_argument(
+        "--github_repos",
+        help="The comma-separated repo slugs of the remotes on GitHub.",
+        default="bitcoin-core/gui,bitcoin/bitcoin",
+    )
     parser.add_argument('--dry_run', help='Print changes/edits instead of calling the GitHub API.', action='store_true', default=False)
     args = parser.parse_args()
 
     args.scratch_dir = os.path.join(args.scratch_dir, '')
     os.makedirs(args.scratch_dir, exist_ok=True)
-    repo_dir = os.path.join(args.scratch_dir, args.github_repo)
+    for slug in args.github_repos.split(","):
+        repo_dir = os.path.join(args.scratch_dir, slug)
 
-    url = 'https://github.com/{}'.format(args.github_repo)
-    if not os.path.isdir(repo_dir):
-        print('Clone {} repo to {}'.format(url, repo_dir))
-        os.chdir(args.scratch_dir)
-        call_git(['clone', '--quiet', url, repo_dir])
-        print('Set git metadata')
+        url = 'https://github.com/{}'.format(slug)
+        if not os.path.isdir(repo_dir):
+            print('Clone {} repo to {}'.format(url, repo_dir))
+            os.chdir(args.scratch_dir)
+            call_git(['clone', '--quiet', url, repo_dir])
+            print('Set git metadata')
+            os.chdir(repo_dir)
+            with open(os.path.join(repo_dir, '.git', 'config'), 'a') as f:
+                f.write('[remote "{}"]\n'.format(UPSTREAM_PULL))
+                f.write('    url = {}\n'.format(url))
+                f.write('    fetch = +refs/pull/*:refs/remotes/upstream-pull/*\n')
+                f.flush()
+            call_git(['config', 'user.email', 'no@ne.nl'])
+            call_git(['config', 'user.name', 'none'])
+            call_git(['config', 'gc.auto', '0'])
+
+        print(f'Fetching diffs for {slug} ...')
         os.chdir(repo_dir)
-        with open(os.path.join(repo_dir, '.git', 'config'), 'a') as f:
-            f.write('[remote "{}"]\n'.format(UPSTREAM_PULL))
-            f.write('    url = {}\n'.format(url))
-            f.write('    fetch = +refs/pull/*:refs/remotes/upstream-pull/*\n')
-            f.flush()
-        call_git(['config', 'user.email', 'no@ne.nl'])
-        call_git(['config', 'user.name', 'none'])
-        call_git(['config', 'gc.auto', '0'])
+        call_git(['fetch', '--quiet', '--all'])
 
-    print('Fetching diffs ...')
-    os.chdir(repo_dir)
-    call_git(['fetch', '--quiet', '--all'])
+        print(f'Fetching open pulls for {slug} ...')
+        github_api = Github(args.github_access_token)
+        github_repo = github_api.get_repo(slug)
+        base_name = github_repo.default_branch
+        pulls = return_with_pull_metadata(lambda: [p for p in github_repo.get_pulls(state='open', base=base_name)])
+        call_git(['fetch', '--quiet', '--all'])  # Do it again just to be safe
+        call_git(['fetch', 'origin', '{}'.format(base_name), '--quiet'])
 
-    print('Fetching open pulls ...')
-    github_api = Github(args.github_access_token)
-    github_repo = github_api.get_repo(args.github_repo)
-    pulls = return_with_pull_metadata(lambda: [p for p in github_repo.get_pulls(state='open', base=args.base_name)])
-    call_git(['fetch', '--quiet', '--all'])  # Do it again just to be safe
-    call_git(['fetch', 'origin', '{}'.format(args.base_name), '--quiet'])
+        print(f'Open {base_name}-pulls for {slug}: {len(pulls)}')
+        pulls_mergeable = [p for p in pulls if p.mergeable]
+        print(f'Open mergeable {base_name}-pulls for {slug}: {len(pulls_mergeable)}')
 
-    print('Open {}-pulls: {}'.format(args.base_name, len(pulls)))
-    pulls_mergeable = [p for p in pulls if p.mergeable]
-    print('Open mergeable {}-pulls: {}'.format(args.base_name, len(pulls_mergeable)))
+        with tempfile.TemporaryDirectory() as temp_git_work_tree:
+            shutil.copytree(
+                os.path.join(repo_dir, '.git'),
+                os.path.join(temp_git_work_tree, '.git'),
+            )
+            os.chdir(temp_git_work_tree)
 
-    with tempfile.TemporaryDirectory() as temp_git_work_tree:
-        shutil.copytree(
-            os.path.join(repo_dir, '.git'),
-            os.path.join(temp_git_work_tree, '.git'),
-        )
-        os.chdir(temp_git_work_tree)
+            if args.update_comments:
+                for i, pull_update in enumerate(pulls_mergeable):
+                    print('{}/{} Checking for conflicts {} <> {} <> {} ... '.format(i, len(pulls_mergeable), base_name, pull_update.number, 'other_pulls'))
+                    pulls_conflict = calc_conflicts(pulls_mergeable=pulls_mergeable, num=pull_update.number, base_branch=base_name)
+                    update_comment(dry_run=args.dry_run, pull=pull_update, pulls_conflict=pulls_conflict)
 
-        if args.update_comments:
-            for i, pull_update in enumerate(pulls_mergeable):
-                print('{}/{} Checking for conflicts {} <> {} <> {} ... '.format(i, len(pulls_mergeable), args.base_name, pull_update.number, 'other_pulls'))
-                pulls_conflict = calc_conflicts(pulls_mergeable=pulls_mergeable, num=pull_update.number, base_branch=args.base_name)
-                update_comment(dry_run=args.dry_run, pull=pull_update, pulls_conflict=pulls_conflict)
+            if args.pull_id:
+                pull_merge = [p for p in pulls if p.number == args.pull_id]
 
-        if args.pull_id:
-            pull_merge = [p for p in pulls if p.number == args.pull_id]
+                if not pull_merge:
+                    print('{} not found in all {} open {} pulls'.format(args.pull_id, len(pulls), base_name))
+                    sys.exit(-1)
+                pull_merge = pull_merge[0]
 
-            if not pull_merge:
-                print('{} not found in all {} open {} pulls'.format(args.pull_id, len(pulls), args.base_name))
-                sys.exit(-1)
-            pull_merge = pull_merge[0]
+                if not pull_merge.mergeable:
+                    print('{} is not mergeable'.format(pull_merge.number))
+                    sys.exit(-1)
 
-            if not pull_merge.mergeable:
-                print('{} is not mergeable'.format(pull_merge.number))
-                sys.exit(-1)
+                print(f'Checking for conflicts {base_name} <> {pull_merge.number} <> other_pulls ... ')
+                conflicts = calc_conflicts(pulls_mergeable=pulls_mergeable, num=pull_merge.number, base_branch=base_name)
 
-            print('Checking for conflicts {} <> {} <> {} ... '.format(args.base_name, pull_merge.number, 'other_pulls'))
-            conflicts = calc_conflicts(pulls_mergeable=pulls_mergeable, num=pull_merge.number, base_branch=args.base_name)
+                update_comment(dry_run=args.dry_run, pull=pull_merge, pulls_conflict=conflicts)
 
-            update_comment(dry_run=args.dry_run, pull=pull_merge, pulls_conflict=conflicts)
-
-        os.chdir(repo_dir)
+            os.chdir(repo_dir)
 
 
 if __name__ == '__main__':
