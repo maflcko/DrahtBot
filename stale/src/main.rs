0a2e0ce248d17c946dd8cc00fd91c2f1f24e09a0
MarcoFalke <falke.marco@gmail.com>
2022-10-31 14:20:27 +0100

    stale: Combine rebase label into stale

--- stale/src/main.rs
+++ stale/src/main.rs
@@ -1,7 +1,11 @@
 use clap::Parser;
 
 #[derive(clap::Parser)]
-#[command(about = "Comment on pull requests that needed a rebase for too long.", long_about = None)]
+#[command(about = "\
+Handle stale issues and pull requests:
+* Comment on pull requests that needed a rebase for too long.\n\
+* Update the label that indicates a rebase is required.\n\
+", long_about = None)]
 struct Args {
     /// The access token for GitHub.
     #[arg(long)]
@@ -69,6 +73,102 @@ async fn stale(
                 issues_api
                     .create_comment(item.number.try_into().unwrap(), text)
                     .await?;
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn rebase_label(
+    github: &octocrab::Octocrab,
+    config: &Config,
+    github_repo: &Vec<util::Slug>,
+    dry_run: bool,
+) -> octocrab::Result<()> {
+    let id_needs_rebase_comment = util::IdComment::NeedsRebase.str();
+    let id_stale_comment = util::IdComment::Stale.str();
+
+    for util::Slug { owner, repo } in github_repo {
+        println!("Get open pulls for {}/{} ...", owner, repo);
+        let issues_api = github.issues(owner, repo);
+        let pulls_api = github.pulls(owner, repo);
+        let pulls = github
+            .all_pages(
+                pulls_api
+                    .list()
+                    .state(octocrab::params::State::Open)
+                    .send()
+                    .await?,
+            )
+            .await?;
+        println!("Open pulls: {}", pulls.len());
+        for (i, pull) in pulls.iter().enumerate() {
+            println!(
+                "{}/{} (Pull: {}/{}#{})",
+                i,
+                pulls.len(),
+                owner,
+                repo,
+                pull.number
+            );
+            let pull = util::get_pull_mergeable(&pulls_api, pull.number).await?;
+            let pull = match pull {
+                None => {
+                    continue;
+                }
+                Some(p) => p,
+            };
+            let mut labels = github
+                .all_pages(issues_api.list_labels_for_issue(pull.number).send().await?)
+                .await?;
+            let found_label_rebase = labels
+                .iter()
+                .find(|l| l.name == config.label_needs_rebase)
+                .is_some();
+            if pull.mergeable.unwrap() {
+                if found_label_rebase {
+                    println!("... remove label '{}')", config.label_needs_rebase);
+                    let all_comments = github
+                        .all_pages(issues_api.list_comments(pull.number).send().await?)
+                        .await?;
+                    let comments = all_comments
+                        .iter()
+                        .filter(|c| {
+                            let b = c.body.as_ref().unwrap();
+                            b.starts_with(id_needs_rebase_comment)
+                                || b.starts_with(id_stale_comment)
+                        })
+                        .collect::<Vec<_>>();
+                    println!("... delete {} comments", comments.len());
+                    if !dry_run {
+                        labels = issues_api
+                            .remove_label(pull.number, &config.label_needs_rebase)
+                            .await?;
+                        for c in comments {
+                            issues_api.delete_comment(c.id).await?;
+                        }
+                    }
+                }
+            } else {
+                if !found_label_rebase {
+                    println!("... add label '{}'", config.label_needs_rebase);
+                    if !dry_run {
+                        labels = issues_api
+                            .add_labels(pull.number, &[config.label_needs_rebase.to_string()])
+                            .await?;
+                        let text = format!(
+                            "{} \n\
+                            🐙 This pull request conflicts with \
+                            the target branch and [needs rebase]\
+                            (https://github.com/{}/{}/blob/master\
+                            /CONTRIBUTING.md#rebasing-changes).",
+                            id_needs_rebase_comment.to_owned(),
+                            owner,
+                            repo
+                        );
+                        issues_api.create_comment(pull.number, text).await?;
+                    }
+                }
             }
         }
     }
