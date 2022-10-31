43d87593b89d8a5f8aa33cb1785aea5f073e1f28
MarcoFalke <falke.marco@gmail.com>
2022-10-31 11:07:12 +0100

    stale: Initial rust rewrite (💤)

--- stale/src/main.rs
+++ stale/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 
 #[derive(clap::Parser)]
-#[command(about = "Lock discussion on inactive closed issues and pull requests.", long_about = None)]
+#[command(about = "Comment on pull requests that needed a rebase for too long.", long_about = None)]
 struct Args {
     /// The access token for GitHub.
     #[arg(long)]
@@ -9,9 +9,9 @@ struct Args {
     /// The repo slugs of the remotes on GitHub. Format: owner/repo
     #[arg(long)]
     github_repo: Vec<util::Slug>,
-    /// Lock a closed issue or pull request after this many days of inactivity
-    #[arg(long, default_value_t = 365)]
-    inactive_days: i64,
+    /// Comment on a pull request needing rebase after this many days of inactivity
+    #[arg(long, default_value_t = 90)]
+    inactive_rebase_days: i64,
     /// Print changes/edits instead of calling the GitHub API.
     #[arg(long, default_value_t = false)]
     dry_run: bool,
@@ -19,21 +19,24 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> octocrab::Result<()> {
+    let id_stale_comment = util::IdComment::Stale.str();
+
     let args = Args::parse();
 
     let github = util::get_octocrab(args.github_access_token)?;
 
-    let cutoff = { chrono::Utc::now() - chrono::Duration::days(args.inactive_days) }.format("%F");
-    println!("Locking before date {} ...", cutoff);
+    let cutoff =
+        { chrono::Utc::now() - chrono::Duration::days(args.inactive_rebase_days) }.format("%F");
+    println!("Mark stale before date {} ...", cutoff);
 
     for util::Slug { owner, repo } in args.github_repo {
-        println!("Get closed issues and pull requests for {owner}/{repo} ...");
+        println!("Get stale pull requests for {owner}/{repo} ...");
         let items = github
             .all_pages(
                 github
                     .search()
                     .issues_and_pull_requests(&format!(
-                        "repo:{owner}/{repo} is:unlocked is:closed updated:<={cutoff}"
+                        "repo:{owner}/{repo} is:open is:pr label:\"Needs rebase\" updated:<={cutoff}"
                     ))
                     .send()
                     .await?,
@@ -49,9 +52,15 @@ async fn main() -> octocrab::Result<()> {
                 repo,
                 item.number,
             );
+            let text = id_stale_comment.to_owned()
+                + "There hasn't been much activity lately and the patch still needs rebase. What is the status here?\n"
+                + "\n"
+                + "* Is it still relevant? ➡️ Please solve the conflicts to make it ready for review and to ensure the CI passes.\n"
+                + "* Is it no longer relevant? ➡️ Please close.\n"
+                + "* Did the author lose interest or time to work on this? ➡️ Please close it and mark it 'Up for grabs' with the label, so that it can be picked up in the future.\n";
             if !args.dry_run {
                 issues_api
-                    .lock(item.number.try_into().unwrap(), None)
+                    .create_comment(item.number.try_into().unwrap(), text)
                     .await?;
             }
         }
