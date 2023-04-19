4efbae1536764383eadf4ca3b3def7ba433efc2e
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-04-19 12:08:01 +0200

    hacky workaround

--- webhook_features/src/features/ci_status.rs
+++ webhook_features/src/features/ci_status.rs
@@ -54,32 +54,70 @@ impl Feature for CiStatusFeature {
                     == payload["check_suite"]["conclusion"]
                         .as_str()
                         .ok_or(DrahtBotError::KeyNotFound)?;
-                let pulls = payload["check_suite"]["pull_requests"]
-                    .as_array()
-                    .ok_or(DrahtBotError::KeyNotFound)?;
-                let issues_api = ctx.octocrab.issues(repo_user, repo_name);
-                for p in pulls {
-                    let pull_number = p["number"].as_u64().ok_or(DrahtBotError::KeyNotFound)?;
+                let pull_number = {
+                    // Hacky way to get the pull number. See also https://github.com/bitcoin/bitcoin/issues/27178#issuecomment-1503475232
+                    let suite_id = payload["check_suite"]["id"]
+                        .as_u64()
+                        .ok_or(DrahtBotError::KeyNotFound)?;
+                    let checks_api = ctx.octocrab.checks(repo_user, repo_name);
+                    let cirrus_task_id = checks_api
+                        .list_check_runs_in_a_check_suite(suite_id.into())
+                        .per_page(1)
+                        .send()
+                        .await?
+                        .check_runs
+                        .first()
+                        .ok_or(DrahtBotError::KeyNotFound)?
+                        .details_url
+                        .split('/')
+                        .last()
+                        .ok_or(DrahtBotError::KeyNotFound)?
+                        .to_string();
 
-                    let labels = ctx
-                        .octocrab
-                        .all_pages(issues_api.list_labels_for_issue(pull_number).send().await?)
-                        .await?;
-                    let found_label = labels.into_iter().any(|l| l.name == ci_failed_label);
-                    if found_label && success {
-                        println!("... {} remove label '{}')", pull_number, ci_failed_label);
-                        if !ctx.dry_run {
-                            issues_api
-                                .remove_label(pull_number, &ci_failed_label)
-                                .await?;
-                        }
-                    } else if !found_label && !success {
-                        println!("... {} add label '{}'", pull_number, ci_failed_label);
-                        if !ctx.dry_run {
-                            //issues_api
-                            //    .add_labels(pull_number, &[ci_failed_label.to_string()])
-                            //    .await?;
-                        }
+                    let query = format!(
+                        r#"{{ "query": "query GetTaskDetailsById($taskId: ID!) {{ task(id: $taskId) {{ id build {{ id repository {{ owner name }} pullRequest }} }} }}", "variables": {{ "taskId": "{}" }} }}"#,
+                        cirrus_task_id
+                    );
+
+                    let response = reqwest::Client::new()
+                        .post("https://api.cirrus-ci.com/graphql")
+                        .header("Content-Type", "application/json")
+                        .body(query)
+                        .send()
+                        .await
+                        .map_err(|_| DrahtBotError::KeyNotFound)?;
+
+                    response
+                        .json::<serde_json::Value>()
+                        .await
+                        .map_err(|_| DrahtBotError::KeyNotFound)?["data"]["task"]["build"]
+                        ["pullRequest"]
+                        .as_u64()
+                        .ok_or(DrahtBotError::KeyNotFound)?
+                };
+                let issues_api = ctx.octocrab.issues(repo_user, repo_name);
+                let issue = issues_api.get(pull_number).await?;
+                if issue.state != octocrab::models::IssueState::Open {
+                    return Ok(());
+                };
+                let labels = ctx
+                    .octocrab
+                    .all_pages(issues_api.list_labels_for_issue(pull_number).send().await?)
+                    .await?;
+                let found_label = labels.into_iter().any(|l| l.name == ci_failed_label);
+                if found_label && success {
+                    println!("... {} remove label '{}')", pull_number, ci_failed_label);
+                    if !ctx.dry_run {
+                        issues_api
+                            .remove_label(pull_number, &ci_failed_label)
+                            .await?;
+                    }
+                } else if !found_label && !success {
+                    println!("... {} add label '{}'", pull_number, ci_failed_label);
+                    if !ctx.dry_run {
+                        issues_api
+                            .add_labels(pull_number, &[ci_failed_label.to_string()])
+                            .await?;
                     }
                 }
             }
