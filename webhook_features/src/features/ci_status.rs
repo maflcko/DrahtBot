0c4b4a66f4f25317bf4208d50d1b979eb8f281fb
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2024-01-12 17:42:44 +0100

    🚧: lint

--- webhook_features/src/features/ci_status.rs
+++ webhook_features/src/features/ci_status.rs
@@ -131,7 +131,9 @@ impl Feature for CiStatusFeature {
                         // (functional tests are ignored due to intermittent issues)
                         if let Some(first_fail) = check_runs.iter().find(|r| {
                             let text = r.output.text.clone().unwrap_or_default();
-                            text.contains("make: *** [Makefile") || text.contains("clang-tidy-")
+                            text.contains("make: *** [Makefile")
+                                || text.contains("clang-tidy-")
+                                || text.contains("ailure generated from")
                         }) {
                             let comment = format!(
                                 "{}\n{}\n<sub>Debug: {}</sub>",
