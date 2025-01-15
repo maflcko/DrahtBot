02f6f369a28d211149dbb2a3ca68943225369888
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-01-15 18:21:31 +0100

    scripts: move-only

--- scripts/guix.py
+++ scripts/guix.py
@@ -7,9 +7,46 @@ import argparse
 import os
 import sys
 import tempfile
+import re
 import subprocess
+from collections import defaultdict
 
-from util.util import return_with_pull_metadata, call_git, get_git, calculate_table
+from util.util import call_git, get_git
+
+def return_with_pull_metadata(get_pulls):
+    pulls = get_pulls()
+    pulls_update_mergeable = lambda: [p for p in pulls if p.mergeable is None and not p.merged]
+    print('Fetching open pulls metadata ...')
+    while pulls_update_mergeable():
+        print('Update mergable state for pulls {}'.format([p.number for p in pulls_update_mergeable()]))
+        [p.update() for p in pulls_update_mergeable()]
+        pulls = get_pulls()
+    return pulls
+
+
+def calculate_table(base_folder, commit_folder, external_url, base_commit, commit):
+    rows = defaultdict(lambda: ['', ''])  # map from abbrev file name to list of links
+    for f in sorted(os.listdir(base_folder)):
+        short_file_name = re.sub(r'(bitcoin-)?[a-f0-9]{12}', '*', f)
+        os.chdir(base_folder)
+        left = rows[short_file_name]
+        left[0] = '[`{}...`]({}{}/{})'.format(subprocess.check_output(['sha256sum', f], universal_newlines=True)[:16], external_url, base_commit, f)
+        rows[short_file_name] = left
+
+    for f in sorted(os.listdir(commit_folder)):
+        short_file_name = re.sub(r'(bitcoin-)?[a-f0-9]{12}', '*', f)
+        os.chdir(commit_folder)
+        right = rows[short_file_name]
+        right[1] = '[`{}...`]({}{}/{})'.format(subprocess.check_output(['sha256sum', f], universal_newlines=True)[:16], external_url, commit, f)
+        rows[short_file_name] = right
+
+    text = ''
+    for f in rows:
+        text += '| {} | {} | {} |\n'.format(f, rows[f][0], rows[f][1])
+    text += '\n'
+    return text
+
+
 
 ID_GUIX_COMMENT = '<!--9cd9c72976c961c55c7acef8f6ba82cd-->'
 UPSTREAM_PULL = 'upstream-pull'
--- scripts/util/util.py
+++ scripts/util/util.py
@@ -1,40 +1,4 @@
-from collections import defaultdict
-import re
-import os
 import subprocess
-
-def return_with_pull_metadata(get_pulls):
-    pulls = get_pulls()
-    pulls_update_mergeable = lambda: [p for p in pulls if p.mergeable is None and not p.merged]
-    print('Fetching open pulls metadata ...')
-    while pulls_update_mergeable():
-        print('Update mergable state for pulls {}'.format([p.number for p in pulls_update_mergeable()]))
-        [p.update() for p in pulls_update_mergeable()]
-        pulls = get_pulls()
-    return pulls
-
-
-def calculate_table(base_folder, commit_folder, external_url, base_commit, commit):
-    rows = defaultdict(lambda: ['', ''])  # map from abbrev file name to list of links
-    for f in sorted(os.listdir(base_folder)):
-        short_file_name = re.sub(r'(bitcoin-)?[a-f0-9]{12}', '*', f)
-        os.chdir(base_folder)
-        left = rows[short_file_name]
-        left[0] = '[`{}...`]({}{}/{})'.format(subprocess.check_output(['sha256sum', f], universal_newlines=True)[:16], external_url, base_commit, f)
-        rows[short_file_name] = left
-
-    for f in sorted(os.listdir(commit_folder)):
-        short_file_name = re.sub(r'(bitcoin-)?[a-f0-9]{12}', '*', f)
-        os.chdir(commit_folder)
-        right = rows[short_file_name]
-        right[1] = '[`{}...`]({}{}/{})'.format(subprocess.check_output(['sha256sum', f], universal_newlines=True)[:16], external_url, commit, f)
-        rows[short_file_name] = right
-
-    text = ''
-    for f in rows:
-        text += '| {} | {} | {} |\n'.format(f, rows[f][0], rows[f][1])
-    text += '\n'
-    return text
 
 
 def call_git(args, **kwargs):
